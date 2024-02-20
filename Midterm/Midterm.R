5d10a70cd9cdc39196064f560e4b7df7d468a726
Squash28 <Sydneylcole@outlook.com>
2024-02-20 05:05:16 -0500

    Update

--- Midterm/Midterm.R
+++ Midterm/Midterm.R
@@ -1,3 +1,4 @@
+#Installing the necessary packages 
 library(Biostrings)
 library(seqinr)
 library(phangorn)
@@ -5,28 +6,37 @@ library(tidyr)
 library(dplyr)
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-
 BiocManager::install("msa") 
-a
+`force = TRUE`
 library(msa)
 
+#set-up a working directory which will keep all my files in the same place and easy located on my computer
+
 setwd("/Users/Sydne/OneDrive/Desktop/Github_Bio/Bioinformatic/Midterm")
-seqs <- system.file("/Users/Sydne/OneDrive/Desktop/Github_Bio/Bioinformatic/Midterm/sequences.fasta")
 
-sequences <- c("sequences.fasta")
+#command readDNAStringset creates a string for the fasta file. folder and name of the fasta file is provided. Transfers the data from the fasta file to a variable named 'seq'
 
-seqaln <- msaClustalW(sequences)
+seq <- readDNAStringSet("Seq/sequences.fasta")
 
-readDNAStringSet(dna,sequences.fasta)
+#command msaClustalW aligns the DNA sequences of the fasta file using a program? ClustalW. Has a new variable name of 'seqaln'
+#command nchar provides the amount of bp nucleotides in the sequence 
+# alFreq provides a matrix comparing amount of A,T,C,G nucleotides to each individual sequence
+seqaln <- msaClustalW(seq)
+nchar(seqaln)
+alFreq <- alphabetFrequency(seqaln)
 
+#Translates the DNA fasta file to an Amino acid file. New variable of 'seq_AA'
+#print 'seq_AA' will paste the the amino acids of all 20 sequences into the console 
+#Command seq_AA[["Homo_sapiens_6"]] just views the amino acids from the 6th sequence 
+seq_AA <- Biostrings::translate(seq)
+print(seq_AA)
+seq_AA[["Homo_sapiens_6"]]
 
-fas <- ("/Users/Sydne/OneDrive/Desktop/Github_Bio/Bioinformatic/Midterm")
-fas <- system.file("extdata","sequences.fas", package="DECIPHER")
-dna <- readDNAStringSet(fas)
+#Creates a new Fasta file with the aligned DNA of all 20 sequences
+SeqAln_phyDat <- msaConvert(seqaln, type="phangorn::phyDat")
+write.phyDat(SeqAln_phyDat, "Seq/Homo_Sapien6_alignment.fasta", format = "fasta")
 
-AA <- AlignTranslation(dna, type="AAStringSet") # align the translation
-BrowseSeqs(AA, highlight=1) # view the alignment
-DNA <- AlignSeqs(dna)
-
+#Creates a new Fasta file with the translated Amino acid only of the 6th sequence 
+write.fasta(sequences = seq_AA, names = attr(seq_AA,"Homo_sapiens_6"), file.out = "seq_AA_6.fasta")
 
 
